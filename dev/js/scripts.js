8b4e6181c20b34fe5f7fe540663c5c1437894bfb
normand-e <102610415+normand-e@users.noreply.github.com>
2023-05-09 19:17:26 -0400

    filed scrolltrigger

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,5 +1,5 @@
 import { gsap } from "gsap";
-import {ScrollTrigger} from "gsap/ScrollTrigger";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -7,17 +7,25 @@ gsap.registerPlugin(ScrollTrigger);
 
 function heroAnimation(){
     let tl = gsap.timeline();
+    
     tl.from("#alanis-hero", {duration:2, alpha:0, y:200, ease:"back.out(2)"});
     return tl;
 }
 
 function studioAnimation(){
-    let tl = gsap.timeline();
-    tl.from("#albums", {duration:1.5, scale:.25}, "alb");
-    tl.from("#studio-list", {duration:3, alpha:0, x:1000, ease:"power4.out"}, "alb");
-    tl.from("#studio-label", {duration:1.5, alpha:0, y:100, ease:"power3.out"}, "-=1");
+    let tl = gsap.timeline({
+        ScrollTrigger:{
+            trigger:"#studio-list",
+            start:"center 90%",
+            end:"center 10%",
+            scrub:true,
+            markers:true
+        }});
+    //tl.from("#albums", {duration:1.5, scale:.25}, "alb");
+   tl.from("#studio-list", {duration:3, alpha:0, x:1000, ease:"power4.out"}, "alb");
+    //tl.from("#studio-label", {duration:1.5, alpha:0, y:100, ease:"power3.out"}, "-=1");
     
-    return tl;
+    //return tl;
 }
 
 
