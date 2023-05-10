d87398b0d237348c7b5fd1e680db05d1ecca539d
normand-e <102610415+normand-e@users.noreply.github.com>
2023-05-09 22:13:03 -0400

    comments

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,14 +3,14 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-function headText(){
+function headText(){ //hero text section1 animation
     let tl = gsap.timeline();
     tl.from("#alanis-hero", {duration:2, alpha:0, y:200, ease:"back.out(2)"});
 
     return tl;
 }
 
-function heroAnimation(){
+function heroAnimation(){ //change opacity of background hero image
     let tl = gsap.timeline({
         scrollTrigger:{
             trigger:"#section-1",
@@ -25,7 +25,7 @@ function heroAnimation(){
     return tl;
 }
 
-function studioAnimation(){
+function studioAnimation(){ //scroll in text section2
     let tl = gsap.timeline({
         scrollTrigger:{
             trigger:"#section-2",
@@ -41,14 +41,14 @@ function studioAnimation(){
     return tl;
 }
 
-function albumImagesAnim(){
+function albumImagesAnim(){ //scroll trigger scale change album images
     let tl = gsap.timeline({
         scrollTrigger:{
             trigger:".albImg",
             start:"top 100%",
             end:"bottom bottom",
             scrub:true,
-            markers:true,
+            markers:false,
             toggleActions: "play pause resume reset"
         }});
     tl.from(".albImg", {scale:.5});
@@ -56,7 +56,7 @@ function albumImagesAnim(){
     return tl;
 }
 
-function albumText(){
+function albumText(){ //hover animation for album names
     let tl = gsap.timeline();
     let text = document.querySelector("#albName1, #albName2, #albName3, #albName4, #albName5, #albName6, #albName7, #albName8, #albName9, #albName10");
     let animation = gsap.to("#albName1, #albName2, #albName3, #albName4, #albName5, #albName6, #albName7, #albName8, #albName9, #albName10", {
