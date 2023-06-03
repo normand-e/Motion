b1046f5342249d16982cd07c31f944fa03df5b47
normand-e <102610415+normand-e@users.noreply.github.com>
2023-06-03 13:48:33 -0400

    eh

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,7 +6,13 @@ import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 gsap.registerPlugin(DrawSVGPlugin, GSDevTools, MorphSVGPlugin);
 
 
+const mainTL = gsap.timeline({id:"mainTL"});
 
+
+mainTL.from("#bursty-thing", {duration: 1, drawSVG:"100%"})
+
+
+;
 
 
 //GSDevTools.create();
