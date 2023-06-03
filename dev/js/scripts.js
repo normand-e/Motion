31d49850711bcb31ae49f462429b9bf63055cece
normand-e <102610415+normand-e@users.noreply.github.com>
2023-06-03 13:41:34 -0400

    added my svg

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,17 +6,6 @@ import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 gsap.registerPlugin(DrawSVGPlugin, GSDevTools, MorphSVGPlugin);
 
 
-const mainTL = gsap.timeline({id:"mainTL"});
-//drawSVG:"100%" == drawSVG:"true" == drawSVG:"0% 100%"
-mainTL.from("#rect", {duration:1, drawSVG:"50% 50%"})
-.from("#rect", {duration:1, fill:"#fff"},"-=0.75")
-.to("#rect", {duration:1, morphSVG:"#star", fill:"#ff0000" })
-.to("#rect", {duration:1, morphSVG:"#circ", fill:"#0060ef" })
-.from(["#l1","#l2"], {duration:1, drawSVG:"0%", ease:"back.out"},0)
-
-
-;
-
 
 
 
