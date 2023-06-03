e5c560621bb2e33a396e4ed5b32452eae1b597e8
normand-e <102610415+normand-e@users.noreply.github.com>
2023-06-03 15:22:14 -0400

    workin

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -9,10 +9,14 @@ gsap.registerPlugin(DrawSVGPlugin, GSDevTools, MorphSVGPlugin);
 const mainTL = gsap.timeline({id:"mainTL"});
 
 
-mainTL.from("#bursty-thing", {duration: 1, drawSVG:"100%"})
+mainTL.to("#bursty-thing", {duration: 3, drawSVG:"100%", transformOrigin:"50% 50%", rotate:400, x:280, y:190}, "start")
+mainTL.to("#heart", {duration: 2.5, drawSVG:"100%", transformOrigin:"50% 100%", x:-165, y:-210}, "start")
+mainTL.to("#heart", {duration:2.5, scale:.75}, "start")
+mainTL.to("#heart", {duration:.25, scale:.9})
+mainTL.to("#heart", {duration:.25, scale:.75})
 
 
 ;
 
 
-//GSDevTools.create();
+GSDevTools.create({animation: mainTL});
