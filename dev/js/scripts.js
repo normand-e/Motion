1ad296331c4d83ec2d87dd1325a915f43204ebd1
normand-e <102610415+normand-e@users.noreply.github.com>
2023-06-07 17:22:11 -0400

    fixed slide

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -12,6 +12,9 @@ mainTL.set("#slide-middle", {autoAlpha:0})
 mainTL.set("#slide-left", {autoAlpha:0})
 mainTL.set("#trulia-text", {x:-25, y:-300, autoAlpha:0})
 mainTL.set("#apartment-dark", {y:-285, x:2})
+mainTL.set("#long-road", {alpha:0})
+
+
 mainTL.to("#bursty-thing", {duration: 3.5, drawSVG:"100%", transformOrigin:"50% 50%", rotate:360, x:280, y:190, ease: "power3.out"}, "start")
 mainTL.to("#heart", {duration: 2.5, drawSVG:"100%", transformOrigin:"50% 100%", x:-165, y:-210, ease: "power1.out"}, "start")
 mainTL.to("#heart", {duration:2.5, scale:.75, ease: "power3.out"}, "start")
@@ -21,9 +24,11 @@ mainTL.to("#apartment", {duration:1, drawSVG:"100%", scaleX: 0,transformOrigin: 
 mainTL.to("#apts", {duration: 2, x:280, y:-120, scale:.25,  ease: "power1.out"}, "start")
 mainTL.to("#Union_2", {duration: 2.75, drawSVG:"100%",transformOrigin:"50% 100%", x:-210, scale:.1,  ease: "power1.out"}, "start") //outer tree
 mainTL.to("#Union", {duration: 2.2, drawSVG:"100%",transformOrigin:"50% 100%", x:-210, scale:.1,  ease: "power1.out"}, "start+=.85") //inner tree
-mainTL.to("#slide-right", {duration:2.5, x:-205, y:260, scale:.25, ease: "power1.out"}, "start")
-mainTL.to("#slide", {duration:.5, scaleX: 0,transformOrigin: "100% center", ease: "sine.inOut"}, "start")
-mainTL.to("#slide", {duration:.5, x:-50, scaleX:-1,transformOrigin: "top right", ease: "sine.inOut"}, "start+=.5")
+//mainTL.to("#slide-right", {duration:2.5, x:-205, y:260, scale:.25, ease: "power1.out"}, "start")
+mainTL.to("#slide", {duration:2, skewX:60, x:"-=200", y:"+=210", scale:.25}, "start")
+mainTL.to("#ladder", {duration:2, x:"-=270", y:"+=240", scale:.25}, "start")
+//mainTL.to("#slide", {duration:.5, scaleX: 0,transformOrigin: "100% center", ease: "sine.inOut"}, "start")
+//mainTL.to("#slide", {duration:.5, x:-50, scaleX:-1,transformOrigin: "top right", ease: "sine.inOut"}, "start+=.5")
 //mainTL.to("#slide-right", {duration: .5, drawSVG:"100%", morphSVG:"#bursty-thing"}, "start")
 
 
