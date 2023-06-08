406d863ed3bd1ee1dc3e9665d2a7df4a08e769eb
normand-e <102610415+normand-e@users.noreply.github.com>
2023-06-08 18:24:03 -0400

    ugh

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,22 +8,23 @@ gsap.registerPlugin(DrawSVGPlugin, GSDevTools, MorphSVGPlugin);
 
 const mainTL = gsap.timeline({id:"mainTL"});
 
-mainTL.set("#slide-middle", {autoAlpha:0})
-mainTL.set("#slide-left", {autoAlpha:0})
+
 mainTL.set("#trulia-text", {x:-25, y:-300, autoAlpha:0})
 mainTL.set("#apartment-dark", {y:-285, x:2})
-mainTL.set("#long-road", {alpha:0})
+//mainTL.set("#myClip", {x:407, y:750, fill:"#hf4ff9"})
+//mainTL.set(".roadClip", {clipPath:"#myClip"})
+mainTL.set("#long-road", {alpha:1, x:407, y:780, clipPath:"#myClip"})
 
 
-mainTL.to("#bursty-thing", {duration: 3.5, drawSVG:"100%", transformOrigin:"50% 50%", rotate:360, x:280, y:190, ease: "power3.out"}, "start")
-mainTL.to("#heart", {duration: 2.5, drawSVG:"100%", transformOrigin:"50% 100%", x:-165, y:-210, ease: "power1.out"}, "start")
-mainTL.to("#heart", {duration:2.5, scale:.75, ease: "power3.out"}, "start")
-mainTL.to("#flower", {duration: 2.5, drawSVG:"100%", transformOrigin:"50% 50%", rotate:360, scale:.2, y:190,  ease: "power1.out"}, "start")
-mainTL.to("#mountains", {duration: 2.5, drawSVG:"100%", transformOrigin:"100% 50%", scale:.4, x:200,  ease: "power1.out"}, "start")
-mainTL.to("#apartment", {duration:1, drawSVG:"100%", scaleX: 0,transformOrigin: "100% center", ease: "sine.inOut", yoyo:true}, "start")
+mainTL.to("#bursty-thing", {duration: 2.75, transformOrigin:"50% 50%", rotate:360, x:280, y:190, ease: "power1.out)"}, "start")
+mainTL.to("#heart", {duration: 2, transformOrigin:"50% 100%", x:-165, y:-210, ease: "power1.out", scale:.75}, "start")
+mainTL.to("#flower", {duration: 2.5,transformOrigin:"50% 50%", rotate:360, scale:.2, y:190,  ease: "power1.out"}, "start")
+mainTL.to("#mountains", {duration: 2.5, transformOrigin:"100% 50%", scale:.4, x:200,  ease: "power1.out"}, "start")
+mainTL.to("#apartment", {duration:1, scaleX: 0,transformOrigin: "100% center", ease: "sine.inOut", yoyo:true}, "start")
 mainTL.to("#apts", {duration: 2, x:280, y:-120, scale:.25,  ease: "power1.out"}, "start")
-mainTL.to("#Union_2", {duration: 2.75, drawSVG:"100%",transformOrigin:"50% 100%", x:-210, scale:.1,  ease: "power1.out"}, "start") //outer tree
-mainTL.to("#Union", {duration: 2.2, drawSVG:"100%",transformOrigin:"50% 100%", x:-210, scale:.1,  ease: "power1.out"}, "start+=.85") //inner tree
+mainTL.to("#Union_2", {duration: 2.75, transformOrigin:"50% 100%", x:-210, scale:0,  ease: "power1.out"}, "start") //outer tree
+mainTL.to("#Union", {duration: 2.2, transformOrigin:"50% 100%", x:-210, scale:0,  ease: "power1.out"}, "start+=.85") //inner tree
+//mainTL.to("long-road", {duration:2, clipPath:"url(#myClip)"}, "start")
 //mainTL.to("#slide-right", {duration:2.5, x:-205, y:260, scale:.25, ease: "power1.out"}, "start")
 mainTL.to("#slide", {duration:2, skewX:60, x:"-=200", y:"+=210", scale:.25}, "start")
 mainTL.to("#ladder", {duration:2, x:"-=270", y:"+=240", scale:.25}, "start")
@@ -33,10 +34,10 @@ mainTL.to("#ladder", {duration:2, x:"-=270", y:"+=240", scale:.25}, "start")
 
 
 //mainTL.to("#apartment-dark", {delay: 1, duration:.5, drawSVG:"100%", morphSVG: "#bursty-thing"}, "first")
-mainTL.to("#bursty-thing", {duration: .25, drawSVG:"100%", transformOrigin:"50% 50%", rotate:10}, "second")
-mainTL.to("#heart", {duration:.25, drawSVG:"100%", scale:.9}, "second")
-mainTL.to("#heart", {duration:.25, drawSVG:"100%", scale:.75}, "second")
-mainTL.to("#trulia-text", {duration:.5, drawSVG:"100%",y:-400, autoAlpha:1,  ease: "power1.out"}, "second")
+//mainTL.to("#bursty-thing", {duration: .5, transformOrigin:"50% 50%", rotate:"+=30", ease: "power1.out"}, "second")
+
+mainTL.to("#trulia-text", {duration:.5, y:-400, autoAlpha:1,  ease: "power1.out"}, "first-=2.25")
+mainTL.to("#heart", {duration:.5, scale:.75, repeat:2, ease:"power3.out"}, "second")
 
 
 ;
